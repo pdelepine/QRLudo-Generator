48fa05b5bfd8ae2c36c6e4ea5d3f5f5062bbc877
pdelepine <delepine.py@gmail.com>
2021-12-01 14:19:34 +0100

    Déplacement de `hightlightButtons` dans draw

--- app/rendererProcess/view/seriousGame/script_serious_game.js
+++ app/rendererProcess/view/seriousGame/script_serious_game.js
@@ -165,7 +165,7 @@ var sketch = function (p) {
 		/** Déclaration du bouton de création de TextNode */
 		p.buttonCreateTextNode = p.createButton('T');
 		p.buttonCreateTextNode.position(15, 125);
-		p.buttonCreateTextNode.mousePressed(() => { p.createTextNode(); p.switchButtonState(p.CursorState.CREATENODE); p.getCursor(); p.highlightButtons(); });
+		p.buttonCreateTextNode.mousePressed(() => { p.createTextNode(); p.switchButtonState(p.CursorState.CREATENODE); p.getCursor(); });
 		p.buttonCreateTextNode.size(40);
 		p.buttonCreateTextNode.attribute('title', 'Créer un texte');
 		p.buttonCreateTextNode.style('font-family', '"Times New Roman", Times, serif');
@@ -175,7 +175,7 @@ var sketch = function (p) {
 		/** Déclaration du bouton de création de QuestionNode */
 		p.buttonCreateQuestion = p.createButton('?');
 		p.buttonCreateQuestion.position(15, 165);
-		p.buttonCreateQuestion.mousePressed(() => { p.createQuestionNode(); p.switchButtonState(p.CursorState.CREATENODE); p.getCursor(); p.highlightButtons(); });
+		p.buttonCreateQuestion.mousePressed(() => { p.createQuestionNode(); p.switchButtonState(p.CursorState.CREATENODE); p.getCursor(); });
 		p.buttonCreateQuestion.size(40);
 		p.buttonCreateQuestion.attribute('title', 'Créer une question');
 		p.buttonCreateQuestion.parent("seriousGameDiagram");
@@ -186,7 +186,7 @@ var sketch = function (p) {
 		p.buttonCreateLink.position(15, 205);
 		p.buttonCreateLink.size(40);
 		p.buttonCreateLink.attribute('title', 'Créer un lien');
-		p.buttonCreateLink.mousePressed(() => { p.creatingLink = !p.creatingLink; p.switchButtonState(p.CursorState.CREATELINK); p.getCursor(); p.highlightButtons(); })
+		p.buttonCreateLink.mousePressed(() => { p.creatingLink = !p.creatingLink; p.switchButtonState(p.CursorState.CREATELINK); p.getCursor(); })
 		p.buttonCreateLink.parent('seriousGameDiagram');
 		/** L'icône pour le bouton de création de lien */
 		p.iconLink = p.createElement('i');
@@ -201,7 +201,7 @@ var sketch = function (p) {
 		p.buttonEraser.position(15, 245);
 		p.buttonEraser.size(40);
 		p.buttonEraser.attribute('title', 'Supprimer');
-		p.buttonEraser.mousePressed(() => { p.isErasing = !p.isErasing; p.switchButtonState(p.CursorState.ERASING); p.getCursor(); p.highlightButtons(); });
+		p.buttonEraser.mousePressed(() => { p.isErasing = !p.isErasing; p.switchButtonState(p.CursorState.ERASING); p.getCursor(); });
 		p.buttonEraser.parent('seriousGameDiagram');
 		/** L'icône pour le bouton de suppression */
 		p.iconEraser = p.createElement('i');
@@ -216,7 +216,7 @@ var sketch = function (p) {
 		p.buttonMouseSelection.position(15, 285);
 		p.buttonMouseSelection.size(40);
 		p.buttonMouseSelection.attribute('title', 'Outil de sélection');
-		p.buttonMouseSelection.mousePressed(() => { p.isMovingDiagram = false; p.switchButtonState(p.CursorState.SELECTION); p.getCursor(); p.highlightButtons(); });
+		p.buttonMouseSelection.mousePressed(() => { p.isMovingDiagram = false; p.switchButtonState(p.CursorState.SELECTION); p.getCursor(); });
 		p.buttonMouseSelection.parent('seriousGameDiagram');
 		/** L'icône pour le bouton de sélection */
 		p.iconMouseSelection = p.createElement('i');
@@ -231,7 +231,7 @@ var sketch = function (p) {
 		p.buttonMouseDisplacement.position(15, 325);
 		p.buttonMouseDisplacement.size(40);
 		p.buttonMouseDisplacement.attribute('title', 'Outil de déplacement du dessin');
-		p.buttonMouseDisplacement.mousePressed(() => { p.isMovingDiagram = true; p.getCursor(); p.switchButtonState(p.CursorState.DISPLACEMENT); p.highlightButtons(); });
+		p.buttonMouseDisplacement.mousePressed(() => { p.isMovingDiagram = true; p.getCursor(); p.switchButtonState(p.CursorState.DISPLACEMENT); });
 		p.buttonMouseDisplacement.parent('seriousGameDiagram');
 		/** L'icône pour le bouton de déplacement */
 		p.iconMouseDisplacement = p.createElement('i');
@@ -250,7 +250,6 @@ var sketch = function (p) {
 		p.sliderZoom.mouseReleased(() => { p.sliderNotPressed = true; }); 	//remet à vrai quand on arrête d'utiliser le slider
 		p.sliderZoom.parent("seriousGameDiagram");
 
-		p.highlightButtons();
 	}
 
 	/** La boucle de dessin */
@@ -308,6 +307,8 @@ var sketch = function (p) {
 		// Affiche le pourcentage de zoom auquel on est actuellement
 		p.text(p.sliderZoom.value() + "%", (p.width) - 8, (p.height) - 8);
 		p.pop();
+
+		p.highlightButtons();
 	}
 
 	/** Fonction de dessin de la palette de bouton de création  */
@@ -324,13 +325,13 @@ var sketch = function (p) {
 
 	/** Fonction qui déclenche la création de QuestionNode */
 	p.createQuestionNode = function () {
-		p.hoveringNode = true;
+		p.hoveringNode = !p.hoveringNode;
 		p.creatingNodeType = 'questionNode';
 	}
 
 	/** Fonction qui déclenche la création de TextNode */
 	p.createTextNode = function () {
-		p.hoveringNode = true;
+		p.hoveringNode = !p.hoveringNode;
 		p.creatingNodeType = 'textNode';
 	}
 
@@ -439,7 +440,6 @@ var sketch = function (p) {
 					p.hoveringNode = false;
 					p.switchButtonState(p.CursorState.SELECTION);
 					p.getCursor();
-					p.highlightButtons();
 				}
 			}
 		}
@@ -476,7 +476,6 @@ var sketch = function (p) {
 							l.type = 'static';
 							//p.creatingLink = false;
 							p.getCursor();
-							p.highlightButtons();
 						}
 					});
 				}
