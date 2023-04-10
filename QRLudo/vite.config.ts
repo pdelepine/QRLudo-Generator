c4dcba839a0d0582ca4e55e394721b55de48cdbf
pdelepine <delepine.py@gmail.com>
2023-04-10 22:55:19 +0200

    Config dev et production

--- QRLudo/vite.config.ts
+++ QRLudo/vite.config.ts
@@ -3,13 +3,13 @@ import vue from "@vitejs/plugin-vue";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // Utilities
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import { fileURLToPath, URL } from "node:url";
-import path from "path";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, "./dist/"),
+  base: normalizePath(resolve(__dirname, "./dist")),
   plugins: [
     vue({
       template: { transformAssetUrls },
